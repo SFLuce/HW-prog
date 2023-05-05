b6a3c1f5ea0f33de555cc176f49b0f47dc5e0ab8
Lana <svet-market@mail.ru>
2023-05-05 20:16:27 +0300

    HW1

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿// HW001 Найти максимальное из двух чисел
+
+// Console.WriteLine("Input a");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input b");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int max = a;
+// if (a > b)
+// {
+//     Console.Write($"Max -> {max}");
+// }
+// else
+// {
+//     Console.Write($"Max -> {b}");
+// }
+
+// HW002 Найти максимальное из трех чисел
+
+// Console.WriteLine("Input a");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input b");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input c");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// int max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+// Console.WriteLine($"Max = {max}");
+
+// HW003 Определить четность числа
+
+// Console.WriteLine("Input  number");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// if (n %2 == 0)
+// {
+//     Console. WriteLine($"{n} -> even number");
+// }
+// else
+// {
+//     Console.WriteLine($"{n} -> uneven number");
+// }
+
+// HW004 Вывести все четные числа из ряда от 1 до N 
+
+Console.WriteLine("Input  N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+while (count <= n)
+{
+    if (count %2 == 0)
+    {
+        Console.Write($"{count} ","" );
+     }
+     count++;
+}
+
