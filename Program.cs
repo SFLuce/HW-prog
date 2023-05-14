f6ca0141b4aa82644dd0494335c6b58820bd5a1f
Lana <svet-market@mail.ru>
2023-05-14 17:35:37 +0300

    hw2

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// int SecondDigit (int num)
+// {
+//     int dec = num / 10  % 10;
+//     return dec;
+// }
+// int Input (string input)
+// {
+//     Console.WriteLine(input);
+//     string value = Console.ReadLine();
+//     int result = Convert.ToInt32(value);
+//     return result;
+// }
+// int num = Input("Введите трехзначное число");
+//  if (num > 1000 || num <100)
+//  {
+//     Console.WriteLine("Вы ввели не трехзначное число");
+//     return;
+//  }
+// Console.WriteLine($"{num} -> {SecondDigit(num)}");
+
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// int Week (string day)
+// {
+//     Console.WriteLine(day);
+//     string num = Console.ReadLine();
+//     int result = Convert.ToInt32(num);
+//     return result;
+// }
+// bool WeekEnd (int num)
+// {
+//     if (num > 5) return true;
+//     else return false;
+// }
+// int num = Week("Введите число, обозначающее день недели от 1 до 7");
+// if (num < 0 && num > 7)
+// {
+//     Console.WriteLine("Вы ввели неверное число");
+//     return;
+// }
+// Console.WriteLine($"{num} -> {WeekEnd(num)}");
+
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
