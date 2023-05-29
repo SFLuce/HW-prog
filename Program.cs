f6b210b2678fa8316857b221f82b5336fd56d296
SFLuce <130655408+SFLuce@users.noreply.github.com>
2023-05-29 22:46:05 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,66 +1,96 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Console.WriteLine("Hello, World!");
 
-// Задача 19
-// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-// 14212 -> нет
-// 12821 -> да
-// 23432 -> да
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 
-// bool Palyndrom(int num)
+// [345, 897, 568, 234] -> 2
+
+// int[] CreateArrey(int size)
 // {
-//     if (num/10000 == num % 10 && num/1000 %10 == num/10 % 10) return true;
-//     else return false;
-// }
-// Console.WriteLine("Введите пятизначное число:");
-// int num = Convert.ToInt32(Console.ReadLine());
-// if (num<10000)
-// {
-//     Console.WriteLine("Вы ввели не пятизначное число");
-//     return;
-// }
-// Console.WriteLine($"{num} -> {Palyndrom(num)}");
+//     int[] arrey = new int[size];
 
-// Задача 21
-// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-// A (3,6,8); B (2,1,-7), -> 15.84
-// A (7,-5, 0); B (1,-1,9) -> 11.53
-
-double FindSection(double xa, double ya, double za, double xb, double yb, double zb)
-{
-    return Math.Round(Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2) + Math.Pow(zb - za, 2)),2);
-}
-Console.WriteLine("Input coordinate X to A:");
-double xa = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Input coordinate Y to A:");
-double ya = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Input coordinate Z to A:");
-double za = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Input coordinate X to B:");
-double xb = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Input coordinate Y to B:");
-double yb = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Input coordinate Z to A:");
-double zb = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine($"Length AB -> {FindSection(xa, ya, za, xb, yb, zb)}");
-
-
-
-// Задача 23
-// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-// 3 -> 1, 8, 27
-// 5 -> 1, 8, 27, 64, 125
-
-// void Cube (int num)
-// {
-//     int i = 1;
-//     while (i<=num)
+//     for (int i = 0; i < size; i++)
 //     {
-//         Console.WriteLine($"{i} -> {i*i*i}");
-//         i++;
+//         Console.Write($"Input three-digit element of arrey {i}:  ");
+//         arrey[i]=Convert.ToInt32(Console.ReadLine());
+//     }
+//     return arrey;
+// }
+// int CountEven(int[] arrey)
+// {
+//     int count = 0;
+//     for (int i = 0; i < arrey.Length; i++)
+//     {
+//         if (arrey[i] %2 ==0) count++;
+//     }
+//     return count;
+// }
+// int[] myArrey = CreateArrey(5);
+// Console.WriteLine($"Count even-digit in arrey -> {CountEven(myArrey)}");
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+
+// int[] CreateArrey(int size, int minValue, int maxValue)
+// {
+//     int[] newArrey = new int[size];
+
+//     for (int i = 0; i < size; i++)
+//     {
+//         newArrey[i]= new Random().Next(minValue, maxValue+1);
+//     }
+//     return newArrey;
+// }
+// void PrintArrey(int[] arrey)
+// {
+//     for (int i = 0; i < arrey.Length; i++)
+//     {
+//         Console.Write(arrey[i] + " ");
 //     }
 // }
-// Console.WriteLine("Введите целое число");
-// int num = Convert.ToInt32(Console.ReadLine());
-// Cube(num);
+
+// int SumOddIndex(int[] arrey)
+// {
+//     int sum = 0;
+//     for (int i = 1; i < arrey.Length; i+=2)
+//     {
+//         sum += arrey[i];
+//     }
+//     return sum;
+// }
+
+// int[] myArrey = CreateArrey(10, -100, 100);
+// PrintArrey(myArrey);
+// Console.WriteLine();
+// Console.WriteLine($"Sum elements in odd index -> {SumOddIndex(myArrey)}");
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] CreateArrey(int size, int minValue, int maxValue)
+{
+   double[] newArrey= new double[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        newArrey[i]= rand.Next(minValue, maxValue+1)+ rand.NextDouble();
+    }
+    return newArrey;
+}
+
+void PrintArrey(double[] arrey)
+{
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        Console.Write(arrey[i] + " ");
+    }
+}
+
+
+double[] myArrey = CreateArrey(5, -10, 10);
+
+PrintArrey(myArrey);
+Console.WriteLine();
+Console.WriteLine($"Difference Max & Min elements -> {myArrey.Max()- myArrey.Min()}");
 
