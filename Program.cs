aded41c0c0a6d832efc479c3f1ce716c81018e89
SFLuce <130655408+SFLuce@users.noreply.github.com>
2023-06-18 23:47:27 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,188 +1,63 @@
-﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// В итоге получается вот такой массив:
-// 7 4 2 1
-// 9 5 3 2
-// 8 4 4 2
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
 
-int[,] Create2DRandomArray(int rows, int columns, int minValue, int maxValue)
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void NaturalNumbers(int n)
 {
-    int[,] array = new int[rows, columns];
-        for (int i = 0; i <rows; i++)
+    if (n >0)
     {
-       for (int j = 0; j < columns; j++)
-       {
-            array[i,j]= new Random().Next(minValue, maxValue+1);
-       }
-    }
-    return array;
-}
-
-void Print2DArray(int[,] array)
-{
-    for (int i = 0; i <array.GetLength(0); i++)
-    {
-       for (int j = 0; j < array.GetLength(1); j++)
-       {
-        Console.Write(array[i,j] + "\t");
-       }
-        Console.WriteLine();
-    }  
-}
-
- void SortOfElements(int[,] array)
-{
-    for (int i = 0; i <array.GetLength(0); i++)
-    {
-         for (int j = 0; j < array.GetLength(1); j++)
-       {
-           for (int k = 0; k < array.GetLength(1)-1; k++)
-          {
-                 if (array[i,k] < array[i,k+1]) 
-                {
-                int temp = array[i,k];
-                array[i,k] = array[i,k+1];
-                array[i,k+1] = temp;
-                }
-            }
-         }  
-    } 
-}
-
-int[,] myArray = Create2DRandomArray(4, 5, 0, 30);
-Print2DArray(myArray);
-Console.WriteLine();
-SortOfElements(myArray);
-Print2DArray(myArray);
-
-
-
-
-
-// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// 5 2 6 7
-// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
-
-void NumberOfRowMinSum(int[,] array)
-{
-    int minRow = 0;
-    int minSumRow = 0;
-    int sumRow = 0;
-    for (int j= 0; j < array.GetLength(1); j++)
-    {
-        minRow += array[0, j];
-    }
-    for (int i = 0; i <array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++) sumRow += array[i, j];
-        if (sumRow < minRow)
-        {
-            minRow = sumRow;
-            minSumRow = i;
-        }
-        sumRow = 0;
-    }
-    Console.Write($"Row of minimum sum -> {minSumRow +1}");
-}
-
-int[,] myArray = Create2DRandomArray(5, 4, 0, 10);
-Print2DArray(myArray);
-NumberOfRowMinSum(myArray);
-
-
-
-// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
-// Например, даны 2 матрицы:
-// 2 4 | 3 4
-// 3 2 | 3 3
-// Результирующая матрица будет:
-// 18 20
-// 15 18
-
-int[,] MatrixProdukt(int[,] matrix1, int[,] matrix2)
-{
-    int row1 = matrix1.GetLength(0); int col1 = matrix1.GetLength(1);
-    int row2 = matrix2.GetLength(0); int col2 = matrix2.GetLength(1);
-    int[,] matrix3 = new int[row1,col2];
-    for (int i = 0; i <row1; i++)
-    {
-       for (int j = 0; j < col2; j++)
-       {
-        for (int n = 0; n < col1; n++)
-            {
-               
-                matrix3[i,j] += matrix1[i,n] * matrix2[n,j];
-            }
-       }
-    }
-    return matrix3;
-}
-
-int[,] array1= Create2DRandomArray(2, 2, 0, 10);
-Print2DArray(array1);
-int[,] array2= Create2DRandomArray(2, 2, 0, 10);
-Print2DArray(array2);
-Console.WriteLine();
-Print2DArray(MatrixProdukt(array1, array2));
-
-
-// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
-// Массив размером 2 x 2 x 2
-// 66(0,0,0) 25(0,1,0)
-// 34(1,0,0) 41(1,1,0)
-// 27(0,0,1) 90(0,1,1)
-// 26(1,0,1) 55(1,1,1)
-
-int[,,] Create3DRandomArray(int rows, int columns, int depth, int minValue, int maxValue)
-{
-    int[,,] array = new int[rows, columns, depth];
-        for (int i = 0; i <rows; i++)
-    {
-       for (int j = 0; j < columns; j++)
-       {
-            for (int k = 0; k < depth; k++)
-            {
-            array[i,j,k]= new Random().Next(minValue, maxValue+1);
-            }
-       }
-    }
-    return array;
-}
-
-void Print3DArray(int[,,] array)
-{
-    for (int i = 0; i <array.GetLength(0); i++)
-    {
-       for (int j = 0; j < array.GetLength(1); j++)
-       {
-             for (int k = 0; k < array.GetLength(2); k++)
-             {
-                Console.Write($"{array[i,j,k]}({i},{j} ,{k}) \t");
-             }
-              Console.WriteLine();
-        }
-               Console.WriteLine();
+    Console.Write(n + " ");
+    NaturalNumbers(n-1);
     }
 }
 
- int[,,] myArray = Create3DRandomArray(2, 2, 2, 0, 30);
-Print3DArray(myArray);
+NaturalNumbers(8);
 
 
 
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNaturalNumber(int m, int n)
+{
+     int sum = m;
+     if (m == n) return 0;
+     else 
+    {
+        m++;
+        sum = m + SumNaturalNumber(m,n); 
+        return  sum;
+    }
+ }
+
+Console.WriteLine("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма натуральных чисел от {m} до {n} -> {SumNaturalNumber(m-1,n)}");
 
 
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
 
-// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
-// Например, на выходе получается вот такой массив:
-// 01 02 03 04
-// 12 13 14 05
-// 11 16 15 06
-// 10 09 08 07
+int Ack(int m, int n)
+{
+    if (m == 0)
+    {
+        return n+ 1;
+    }
+    else if (m != 0 && n == 0)
+    {
+        return Ack(m-1, 1);
+    }
+    else
+    return Ack(m-1, Ack(m, n-1));
+} 
+
+Console.Write(Ack(3,3));
+
+
